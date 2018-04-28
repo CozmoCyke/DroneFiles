699e4264f143981eec8ad1d9fee8b234912f7e8e
jhuerta6 <jorgehuertahu@gmail.com>
2018-04-28 12:02:26 -0600

    Added files to control little boi - tello

---
+++ tello/custom/commands.py
@@ -0,0 +1,30 @@
+#Made modifications to API. Prints test; does not detect false flag.
+import tello
+import time
+
+drone = tello.Tello('', 9000, imperial=False)
+
+print(drone.takeoff())
+time.sleep(6)
+
+print(drone.move_up(90))
+time.sleep(2)
+
+print(drone.move_forward(300))
+time.sleep(1)
+
+print(drone.rotate_cw(90))
+time.sleep(2)
+
+print(drone.move_forward(200))
+time.sleep(1)
+
+print(drone.rotate_cw(130))
+time.sleep(2)
+
+print(drone.move_forward(250))
+time.sleep(2)
+
+print(drone.land())
+
+print('Flight time: %s' % drone.get_flight_time())
